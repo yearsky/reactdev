4ebdc452dcdbbd1f7654b8408bb2b40e249fa894
yearsky <lifaelp@gmail.com>
2022-03-29 17:32:33 +0700

    login done

---
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -0,0 +1,16 @@
+// ** Auth Endpoints
+export default {
+  loginEndpoint: 'http://127.0.0.1:8000/api/login',
+  // loginEndpoint: '/jwt/login',
+  registerEndpoint: '/jwt/register',
+  refreshEndpoint: 'http://127.0.0.1:8000/api/refresh',
+  logoutEndpoint: 'https://phpstack-736055-2465409.cloudwaysapps.com/api/logout',
+
+  // ** This will be prefixed in authorization header with token
+  // ? e.g. Authorization: Bearer <token>
+  tokenType: 'Bearer',
+
+  // ** Value of this property will be used as key to store JWT token in storage
+  storageTokenKeyName: 'accessToken',
+  storageRefreshTokenKeyName: 'refreshToken'
+}
